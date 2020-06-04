4dc7d0a3e9f7d9065f97b3364bcea3c58be2d059
H4ssan <hassan.ashraf98@gmail.com>
2020-06-04 19:04:32 +0100

    feat: neccessary plugins & loaders installed & set up

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,4 +1,4 @@
-function handleSubmit(event) {
+ async function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
@@ -6,7 +6,7 @@ function handleSubmit(event) {
     checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
+    await fetch('http://localhost:8080/test')
     .then(res => res.json())
     .then(function(res) {
         document.getElementById('results').innerHTML = res.message
--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,30 @@
+const dotenv = require('dotenv');
+dotenv.config();
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+const aylien = require("aylien_textapi");
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+//app endpoint
+projectData = {};
+
+//set aylien api credentials
+const textapi = new aylien({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+});
+
+
+
 
 const app = express()
+
+app.use(cors())
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(express.static('dist'))
 
@@ -18,6 +40,17 @@ app.listen(8080, function () {
     console.log('Example app listening on port 8080!')
 })
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+app.get('/data', function (req, res) {
+
+    let apiData = [];
+    const urlText = document.getElementById('name').value;
+
+    textapi.sentiment({
+        text: urlText
+    },
+        function (error, response) {
+            if (error === null) {
+                console.log(response);
+            }
+        })
+});
